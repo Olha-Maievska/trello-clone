5804569e38cb69eb6af8445bb27eb4f2928fd9f2
Olha Maievska <olhamaievska91@gmail.com>
2024-03-01 19:30:48 +0100

    install prisma with init migration

---
+++ src/app/api/boards/[id]/route.ts
@@ -0,0 +1,73 @@
+import { updateBoardsDto } from './../dto'
+import { NextResponse } from 'next/server'
+import { createBoardDto } from '../dto'
+import { prisma } from '@/core/prisma'
+
+interface BoardRouteContext {
+  params: {
+    id: string
+  }
+}
+
+export async function PUT(req: Request, { params }: BoardRouteContext) {
+  const { id } = params
+  const bodyRaw = await req.json()
+  const validateBody = createBoardDto.safeParse(bodyRaw)
+
+  if (!validateBody.success) {
+    return NextResponse.json(validateBody.error.issues, {
+      status: 400,
+    })
+  }
+
+  const findBoard = await prisma.boards.findUnique({
+    where: {
+      id,
+    },
+  })
+
+  if (!findBoard) {
+    return NextResponse.json([
+      {
+        code: 'not_found',
+        messages: 'Board not found',
+      },
+    ])
+  }
+
+  const updateBoard = await prisma.boards.update({
+    where: {
+      id,
+    },
+    data: validateBody.data,
+  })
+
+  return NextResponse.json(updateBoard)
+}
+
+export async function DELETE(req: Request, { params }: BoardRouteContext) {
+  const { id } = params
+
+  const findBoard = await prisma.boards.findUnique({
+    where: {
+      id,
+    },
+  })
+
+  if (!findBoard) {
+    return NextResponse.json([
+      {
+        code: 'not_found',
+        messages: 'Board not found',
+      },
+    ])
+  }
+
+  await prisma.boards.delete({
+    where: {
+      id,
+    },
+  })
+
+  return NextResponse.json({}, { status: 200 })
+}
